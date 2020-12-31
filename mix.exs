4e24ab33a4ef3e72b0dd91780d95ea707f049574
IKEDA Hiroki <ikeda_hiroki@icloud.com>
2020-12-31 13:32:20 +0000

    downgrade the required elixir version

--- mix.exs
+++ mix.exs
@@ -5,7 +5,7 @@ defmodule ComplexNumber.MixProject do
     [
       app: :complex_number,
       version: "0.1.0",
-      elixir: "~> 1.11",
+      elixir: "~> 1.10",
       start_permanent: Mix.env() == :prod,
       deps: deps(),
       test_coverage:
